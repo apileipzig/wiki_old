38fcfb18a436881e9529dd1f33c22c037720b3c1
ben <ben@nerdlabor.de>
2011-08-14 13:32:09 +0200

    added output messages to wiki static file initializer

--- lib/generate_static_files.rb
+++ lib/generate_static_files.rb
@@ -2,7 +2,8 @@ marker_header = '<!-- /#Start_Content_Marker - DO NOT (RE)MOVE! -->'
 marker_footer = '<!-- /#End_Content_Marker - DO NOT (RE)MOVE! -->'
 
 begin
-	html = File.open(Rails.root + '../index.html', 'r') { |f| f.read }
+  puts "Try to generate static files for wiki..."
+  html = File.open(Rails.root + '../index.html', 'r') { |f| f.read }
 
   doc = Nokogiri::HTML(html[0,html.index(marker_header)])
 
@@ -24,6 +25,7 @@ begin
 	write_to.puts(header[0,header.length-16]) #extreme ugly because nokogiri add </body></html> at the end of its document
 
 	write_to = File.new(Rails.root + "app/views/layouts/footer.html", "w+")
+
 	write_to.puts(html[html.index(marker_footer)+marker_footer.length,html.length])
 
   #copy fresh static files from the frontend on every startup only on dev mode
@@ -36,7 +38,7 @@ begin
     %x[cp -R ../images public/]
     %x[touch public/images/.images_go_here]
   end
- 
+  puts "...finished!"
 rescue Exception => e
 	puts "Can't generate static HTML Header and Footer! Do nothing!"
 	puts "ERROR: #{e}"
